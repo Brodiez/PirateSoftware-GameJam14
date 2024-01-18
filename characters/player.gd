af1b6d52df1c87a9708a73b6c592bc4c9639151e
enweave <enweave@gmail.com>
2024-01-19 02:16:59 +0500

    Add mouselook

--- characters/player.gd
+++ characters/player.gd
@@ -1,14 +1,41 @@
 extends CharacterBody3D
 
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 4.5
-
+var wordlspace : PhysicsDirectSpaceState3D 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+const SPEED = 10.0
+const JUMP_VELOCITY = 4.5
+
+
+func get_aim_direction() -> Quaternion:
+	# calculates look direction
+	# mouse logic
+	var aim_direction = self.global_transform.basis.get_rotation_quaternion()
+	var mouse_position = get_viewport().get_mouse_position()
+	var ray = PhysicsRayQueryParameters3D.new()
+	
+	ray.from = $CameraPivot/Camera3D.project_ray_origin(mouse_position)
+	ray.to = ray.from + $CameraPivot/Camera3D.project_ray_normal(mouse_position) * 2000
+	
+	var hit_result = wordlspace.intersect_ray(ray)
+	
+	if hit_result:
+		var _direction = self.global_transform.origin.direction_to(hit_result.position)
+		_direction.y = 0
+		aim_direction = Quaternion(Vector3.FORWARD, _direction)
+		
+	# TODO: gamepad logic
+	return aim_direction
+
+func _ready():
+	wordlspace = get_world_3d().get_direct_space_state()
 
 func _physics_process(delta):
+	# Set player look direction.
+	$Capsule.set_quaternion(get_aim_direction())
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
