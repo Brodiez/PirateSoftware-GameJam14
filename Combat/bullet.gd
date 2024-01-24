85e6cf8330a4903cf890c882456e14213f19ce9b
enweave <enweave@gmail.com>
2024-01-25 02:10:37 +0500

    bullet - ccd, adjustments and liftime

--- Combat/bullet.gd
+++ Combat/bullet.gd
@@ -1,18 +1,20 @@
 extends RigidBody3D
 
 
-var max_collisions : int = 1
-
+var max_collisions : int = 2
+var life_time : float = 0.0
+const KILL_TIME : float = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	life_time = KILL_TIME
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# detect collisions
-	if get_contact_count() >= max_collisions:
+	life_time -= delta
+	if get_contact_count() >= max_collisions or life_time <= 0.0:
 		# destroy the object
 		queue_free()
 
